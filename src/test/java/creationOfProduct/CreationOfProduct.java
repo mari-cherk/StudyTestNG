ddf49fcfb150a6df775f77bf0ca37bf2a85251f4
Марина Черкасова <1mari.cherkasov@gmail.com>
2019-08-12 17:37:54 +0300

    first commit

--- src/test/java/creationOfProduct/CreationOfProduct.java
+++ src/test/java/creationOfProduct/CreationOfProduct.java
@@ -3,6 +3,9 @@ package creationOfProduct;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,19 +13,31 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import java.io.File;
 import java.time.Duration;
 import java.util.Random;
 
+import static java.lang.System.getProperty;
+
 public class CreationOfProduct {
+    WebDriver driver = null;
 
 
-    @Test(dataProvider ="getData")
+    @BeforeTest
+    @Parameters({"browser"})
+    public void beforeTest(String browser) {
+        driver = DriverManager.getDriver("browser");
+    }
+
+
+    @Test(dataProvider = "getData")
     public void signIN(String email, String password) {
-        WebDriver driver = DriverManager.getDriver("chrome");
+
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
         driver.manage().window().maximize();
 
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
 
         WebElement loginInput = driver.findElement(By.id("email"));
@@ -49,17 +64,17 @@ public class CreationOfProduct {
 
         WebElement nameField = driver.findElement(By.id("form_step1_name_1"));
         Random random = new Random();
-        int codeProduct = random.nextInt(1000)+1;
+        int codeProduct = random.nextInt(1000) + 1;
         String codeProductString = Integer.toString(codeProduct);
         nameField.sendKeys(codeProductString);
 
-        int numberProduct = random.nextInt(100)+1;
+        int numberProduct = random.nextInt(100) + 1;
         String numberProductString = Integer.toString(numberProduct);
         WebElement numberField = driver.findElement(By.id("form_step1_qty_0_shortcut"));
         numberField.sendKeys(numberProductString);
 
-        int priceProduct10 = random.nextInt(1000)+1;
-        float priceProduct = priceProduct10/10;
+        int priceProduct10 = random.nextInt(1000) + 1;
+        float priceProduct = priceProduct10 / 10;
         String priceProductString = Float.toString(priceProduct);
         WebElement priceField = driver.findElement(By.id("form_step1_price_shortcut"));
         priceField.sendKeys(priceProductString);
@@ -73,17 +88,12 @@ public class CreationOfProduct {
         configMessage.click();
 
 
-
-
-
-
     }
 
     @DataProvider
-    public Object [][] getData() {
-        return new Object [][] {{"webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw"}};
+    public Object[][] getData() {
+        return new Object[][]{{"webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw"}};
     }
-
 
 
 }
