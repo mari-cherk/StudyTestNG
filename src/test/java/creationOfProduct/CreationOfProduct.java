8d565ef35f6d609c611acf89d181d0b9f9c6cae2
mari-cherk <1mari.cherkasova@gmail.com>
2019-08-12 20:57:22 +0200

    Second commit

--- src/test/java/creationOfProduct/CreationOfProduct.java
+++ src/test/java/creationOfProduct/CreationOfProduct.java
@@ -9,10 +9,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.File;
 import java.time.Duration;
@@ -23,16 +20,20 @@ import static java.lang.System.getProperty;
 public class CreationOfProduct {
     WebDriver driver = null;
 
+    private static final String mCHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int STR_LENGTH = 9;
+    Random random = new Random();
+
 
     @BeforeTest
     @Parameters({"browser"})
-    public void beforeTest(String browser) {
+    public void setBrowser(String browser) {
         driver = DriverManager.getDriver("browser");
     }
 
 
     @Test(dataProvider = "getData")
-    public void signIN(String email, String password) {
+    public void addingProduct(String email, String password) {
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
         driver.manage().window().maximize();
@@ -63,36 +64,81 @@ public class CreationOfProduct {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("form_content")));
 
         WebElement nameField = driver.findElement(By.id("form_step1_name_1"));
-        Random random = new Random();
-        int codeProduct = random.nextInt(1000) + 1;
-        String codeProductString = Integer.toString(codeProduct);
-        nameField.sendKeys(codeProductString);
+        //Random random = new Random();
+
+
+
+        //int nameProduct = random.nextInt(1000) + 1;
+        //String nameProductString = Integer.toString(nameProduct);
+        String nameProductString = createRandomString();
+        nameField.sendKeys(nameProductString);
 
         int numberProduct = random.nextInt(100) + 1;
         String numberProductString = Integer.toString(numberProduct);
         WebElement numberField = driver.findElement(By.id("form_step1_qty_0_shortcut"));
+        numberField.clear();
         numberField.sendKeys(numberProductString);
 
         int priceProduct10 = random.nextInt(1000) + 1;
         float priceProduct = priceProduct10 / 10;
         String priceProductString = Float.toString(priceProduct);
         WebElement priceField = driver.findElement(By.id("form_step1_price_shortcut"));
+        priceField.clear();
         priceField.sendKeys(priceProductString);
         WebElement saveProductButton = driver.findElement(By.xpath("//*[@id=\"form\"]/div[4]/div[2]/div/button[1]"));
         saveProductButton.click();
 
-        //WebElement footerProduct = driver.findElement(By.className())
+        WebElement swichInput = driver.findElement(By.xpath("//*[@id=\"form\"]/div[4]/div[1]/div"));
+        swichInput.click();
+        WebElement configMessage = driver.findElement(By.className("growl growl-notice growl-medium"));
+        configMessage.click();
+
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("growl growl-notice growl-medium")));
-        WebElement configMessage = driver.findElement(By.className("growl growl-notice growl-medium"));
+        configMessage = driver.findElement(By.className("growl growl-notice growl-medium"));
         configMessage.click();
 
 
     }
 
+    //@Test(dataProvider = "getData")
+    //public void checkingProduct(String email, String password) {
+
+        //driver.get("http://prestashop-automation.qatestlab.com.ua/");
+
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+
+        //WebElement loginInput = driver.findElement(By.id("email"));
+        //loginInput.sendKeys(email);
+
+        //WebElement passwordInput = driver.findElement(By.id("passwd"));
+        //passwordInput.sendKeys(password);
+
+        //WebElement submitButton = driver.findElement(By.name("submitLogin"));
+        //submitButton.click();
+
+
+    //}
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+
     @DataProvider
     public Object[][] getData() {
         return new Object[][]{{"webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw"}};
+    }
+
+    public String createRandomString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < STR_LENGTH; i++) {
+            int number = random.nextInt(mCHAR.length());
+            char ch = mCHAR.charAt(number);
+            builder.append(ch);
+        }
+        return builder.toString();
     }
 
 
